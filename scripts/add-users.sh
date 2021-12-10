c27428b80e0c7bc2cd1237ea43dafa0008c28cc0
Vadim Maltsev <fedos550@mail.ru>
2021-12-10 16:22:51 +0300

    Add users.sh

---
+++ scripts/add-users.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#by @izobretatel9
+
+#добавление пользователя Devops
+adduser devops
+usermod -aG sudo devops # добавляем пользователя в sudo
+mkdir -p /home/devops/.ssh #создаем
+echo "" | sudo tee -a /home/devops/.ssh/authorized_keys
+
+#добавление пользователя flant
+adduser flant
+usermod -aG sudo flant # добавляем пользователя в sudo
+mkdir -p /home/flant/.ssh #создаем
+echo "" | sudo tee -a /home/flant/.ssh/authorized_keys
+
+#добавление пользователя research
+adduser research
+usermod -aG sudo research # добавляем пользователя в sudo
+mkdir -p /home/research/.ssh #создаем
+echo "" | sudo tee -a /home/research/.ssh/authorized_keys
+
+sed -i 's/%sudo\s*ALL=(ALL:ALL)\s*ALL\s*/%sudo   ALL=(ALL:ALL) NOPASSWD: ALL/g' /etc/sudoers
+nano /etc/hostname
+nano /etc/hosts
+
+#fdisk -l
+#mkfs.ext4 /dev/vda
+#e2label /dev/vda nvme # Lable
+#echo "LABEL=nvme /mnt/nvme ext4 defaults 0 0" | sudo tee -a /etc/fstab
